1cd9a0bc9e8b999d357063a9b81946e810a27123
SamHara <58473990+SamHara@users.noreply.github.com>
2019-12-12 09:53:14 +0800

    Update resnet18.py

--- resnet18.py
+++ resnet18.py
@@ -114,6 +114,28 @@ def train_loss(x,y):
     optimizer.apply_gradients(zip(grads, model.trainable_variables))
     return loss
 
+@tf.function # 优化
+def test_acc():
+    total_num = 0
+    total_correct = 0
+
+    for x,y in test_db:
+
+        logits = model(x, training=False)
+        prob = tf.nn.softmax(logits, axis=1)
+        pred = tf.argmax(prob, axis=1)
+        pred = tf.cast(pred, dtype=tf.int32)
+
+        correct = tf.cast(tf.equal(pred, y), dtype=tf.int32)
+        correct = tf.reduce_sum(correct)
+
+        total_num += x.shape[0]
+        total_correct += int(correct)
+
+    acc = total_correct / total_num
+    
+    return acc
+
 def preprocess(x, y):
     # 将数据映射到-1~1
     x = 2*tf.cast(x, dtype=tf.float32) / 255. - 1
@@ -123,20 +145,19 @@ def preprocess(x, y):
 (x,y), (x_test, y_test) = datasets.cifar10.load_data() # 加载数据集
 
 y = tf.squeeze(y, axis=1) # 删除不必要的维度
-y_test = tf.squeeze(y_test, axis=1) # 删除不必要的维度
+y_test = tf.squeeze(y_test, axis=1) 
 
 train_db = tf.data.Dataset.from_tensor_slices((x,y)) # 构建训练集
 train_db = train_db.shuffle(1000).map(preprocess).batch(512)
 
 test_db = tf.data.Dataset.from_tensor_slices((x_test,y_test)) #构建测试集
-# 随机打散，预处理，批量化
 test_db = test_db.map(preprocess).batch(512)
 
 sample = next(iter(train_db))
 print('sample:', sample[0].shape, sample[1].shape,tf.reduce_min(sample[0]), tf.reduce_max(sample[0]) ,sample[1].dtype)
 
 #[b, 32, 32, 3] => [b, 1, 1, 512]
-model = resnet18() # 调用函数resnet18，返回一个类，所以，model是一个类
+model = resnet18() # 调用函数resnet18，返回一个类
 model.build(input_shape=(None, 32, 32, 3))
 model.summary()
 summary_writer = tf.summary.create_file_writer('log')
@@ -147,52 +168,35 @@ def main():
 
     
 
-    total_step=list(enumerate(train_db))[-1][0]
+    total_step = list(enumerate(train_db))[-1][0] + 1
 
-    for epoch in range(50): # 训练epoch
+    for epoch in range(60): # 训练epoch
 
         if epoch < 15:
             optimizer.learning_rate = 1e-3
-        elif epoch >= 15 and epoch < 25:
+        elif epoch >= 15 and epoch <= 25:
             optimizer.learning_rate = 1e-4
-        else:
+        elif epoch >= 26 and epoch <= 35:
             optimizer.learning_rate = 1e-5
+        elif epoch >= 36 and epoch <= 45:
+            optimizer.learning_rate = 1e-6
+        else:
+            optimizer.learning_rate = 1e-7
         
 
         for step, (x,y) in enumerate(train_db):
             loss=train_loss(x,y)
 
             with summary_writer.as_default(): 
-             
                 tf.summary.scalar('train-loss', float(loss), step=step+epoch*total_step)
 
             if step % 50 == 0:
                 print(epoch, step, 'loss:', float(loss))
 
-
-
-        total_num = 0
-        total_correct = 0
-
-        for x,y in test_db:
-
-
-            logits = model(x, training=False)
-            prob = tf.nn.softmax(logits, axis=1)
-            pred = tf.argmax(prob, axis=1)
-            pred = tf.cast(pred, dtype=tf.int32)
-
-            correct = tf.cast(tf.equal(pred, y), dtype=tf.int32)
-            correct = tf.reduce_sum(correct)
-
-            total_num += x.shape[0]
-            total_correct += int(correct)
-
-        acc = total_correct / total_num
+        acc = test_acc()
         print(epoch, 'acc:', acc)
 
         with summary_writer.as_default(): 
-            
             tf.summary.scalar('test_acc', float(acc), step=epoch)
 
         tf.saved_model.save(model, 'model_savedmode')
